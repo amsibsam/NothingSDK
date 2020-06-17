2b64fffcea9e496ce6911b5e3896a2b0d52195ca
amsibsam <amsibsam@gmail.com>
2020-06-17 09:38:33 +0700

    Initial commit

---
+++ Example/NothingSDK/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  NothingSDK
+//
+//  Created by amsibsam on 06/17/2020.
+//  Copyright (c) 2020 amsibsam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
